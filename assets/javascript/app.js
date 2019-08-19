5871c3dd8c6c0fcf00a47d410d4fd7d2278a837e
Sheldon Perkins <csperkins18@gmail.com>
2019-08-18 22:03:59 -0400

    <start screen button>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,12 @@
 // Start button page
-
+function startScreen() {
+    var x = document.getElementById("start");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
 // Timer
 
 // Three sets of checkboxs
