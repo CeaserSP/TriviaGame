8c461dd96dbf7691a8580aa8aa7d3912add87542
Sheldon Perkins <csperkins18@gmail.com>
2019-08-19 18:32:57 -0400

    <Score screen inline radial buttons>

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,57 @@
 // Start button page
 function startScreen() {
 
-$("#startButton").click(function () {
-    $(".question").show();
-    $(".button").hide();
-});
+    $("#startButton").click(function () {
+        $(".question").show();
+        $(".button").hide();
+        $(".score").hide();
+    });
 }
-// Timer
 
+// Timer
+var number = 5;
+var intID;
+function run() {
+    intId = setInterval(decrement, 1000);
+}
+
+//  The decrement function.
+function decrement() {
+
+    //  Decrease number by one.
+    number--;
+
+    //  Show the number in the #show-number tag.
+    $(".timer").html("<h2>" + number + "</h2>");
+
+
+    //  Once number hits zero...
+    if (number === 0) {
+
+        //  ...run the stop function.
+        stop();
+
+        //  Alert the user that time is up.
+        scoreScreen();
+    }
+}
+function stop() {
+
+    //  Clears our intervalId
+    //  We just pass the name of the interval
+    //  to the clearInterval function.
+    clearInterval(intId);
+    
+}
+run();
 // Three sets of checkboxs
+
+// Score page
+function scoreScreen() {
+        $(".score").show();
+        $(".question").hide();
+        
+}
 
 // Counter for correct answers page
 
